1eec322e6d1693c682cb2a8b725541d846036c00
Okanclskn <okan35342@gmail.com>
2021-03-14 02:31:58 +0300

    send searchTerm with url

--- pages/components/SearchInput.js
+++ pages/components/SearchInput.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { FormGroup, TextField, Button } from "@material-ui/core";
 import { Form, Formik } from "formik";
-
+import { useRouter } from "next/router";
 const SearchInput = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const router = useRouter();
   const getInputSearchTerm = (event) => {
     setSearchTerm(event.target.value);
   };
   const onFormSubmit = (event) => {
+    router.push("/Home", (query = searchTerm));
     event.preventDefault();
   };
   return (
@@ -22,6 +24,7 @@ const SearchInput = () => {
             value={searchTerm}
             onChange={getInputSearchTerm}
           />
+
           <Button type="submit" variant="contained" color="primary">
             Search
           </Button>
