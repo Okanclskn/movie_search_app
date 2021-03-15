ae5009223d5f14c9c0b37f36ac9f83940bf06b01
Okanclskn <okan35342@gmail.com>
2021-03-15 12:47:56 +0300

    up comings movies request with getStaticProps

--- pages/Home.js
+++ pages/Home.js
@@ -5,8 +5,6 @@ const API_KEY = "dc6f208a79ce2be6fb75d72a554bdd33";
 import movies_api from "./api/movie_api";
 import MovieList from "./components/MoviesList";
 import MovieContext from "./context/UpComingContext";
-import MovieCard from "./components/MovieCard";
-import UpComingMoviesList from "./components/UpComingMoviesList";
 
 export default function HomePage({ movies, trUpComingMovies }) {
   const { upComingMovies, setUpComingMovies } = useContext(MovieContext);
@@ -19,16 +17,9 @@ export default function HomePage({ movies, trUpComingMovies }) {
       <Box margin="0 auto" width="30%">
         <SearchInput></SearchInput>
       </Box>
-      <Grid container wrap="nowrap" spacing={2}>
-        <Grid item xs={3}>
-          <UpComingMoviesList upcomingmovies={upComingMovies} />
-        </Grid>
-        <Grid item xs={9}>
-          <Container maxWidth="md">
-            <MovieList movieslist={movies}></MovieList>
-          </Container>
-        </Grid>
-      </Grid>
+      <Container maxWidth="md">
+        <MovieList movieslist={movies}></MovieList>
+      </Container>
     </React.Fragment>
   );
 }
@@ -42,16 +33,8 @@ HomePage.getInitialProps = async (context) => {
       page: 1,
     },
   });
-  const upcomingresponse = await movies_api.get("/movie/upcoming", {
-    params: {
-      api_key: API_KEY,
-      region: "TR",
-      page: 1,
-    },
-  });
 
   return {
     movies: response.data.results,
-    trUpComingMovies: upcomingresponse.data.results,
   };
 };
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,36 @@
 import Head from "next/head";
 import HomePage from "./Home";
-export default function Home() {
+import { Box, Container, Grid } from "@material-ui/core";
+import movies_api from "./api/movie_api";
+import UpComingMoviesList from "./components/UpComingMoviesList";
+const API_KEY = "dc6f208a79ce2be6fb75d72a554bdd33";
+export default function Home({ trUpComingMovies }) {
   return (
     <div>
       <Head>
         <title>Movies</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <HomePage></HomePage>
+      <Grid container wrap="nowrap" spacing={2}>
+        <Grid item xs={3}>
+          <UpComingMoviesList upcomingmovies={trUpComingMovies} />
+        </Grid>
+        <Grid item xs={9}>
+          <HomePage trUpComingMovies={trUpComingMovies}></HomePage>
+        </Grid>
+      </Grid>
     </div>
   );
+}
+export async function getStaticProps() {
+  const upcomingresponse = await movies_api.get("/movie/upcoming", {
+    params: {
+      api_key: API_KEY,
+      region: "TR",
+      page: 1,
+    },
+  });
+  return {
+    props: { trUpComingMovies: upcomingresponse.data.results },
+  };
 }
